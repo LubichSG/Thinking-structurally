764c0948b9e9ad60e05e001fa7864046afbd9508
Анастасия Шапарева <shaparevaas@outlook.com>
2019-12-04 18:01:53 +0300

    created the project (finally)

---
+++ Team Project/Team Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Team_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
