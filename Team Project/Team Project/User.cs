94464bcd0a1adb949a5e313c02063a76e532bf97
Софья Любич <sglyubich@edu.hse.ru>
2019-12-11 19:25:56 +0300

    Добавила класс user и создала для него конструктор

---
+++ Team Project/Team Project/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Team_Project
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public User(int id, string name, string surname, string phone, string login, string password)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Phone = phone;
+            Login = login;
+            Password = password;
+        }
+
+    }
+}
