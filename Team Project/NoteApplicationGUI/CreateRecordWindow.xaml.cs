b9d12658c34819c7895902fe8f29c6238653998f
Софья Любич <sglyubich@edu.hse.ru>
2019-12-14 00:40:30 +0300

    Создание и заполнение нового окна "Create a new record!"

---
+++ Team Project/NoteApplicationGUI/CreateRecordWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NoteApplicationGUI
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateRecordWindow.xaml
+    /// </summary>
+    public partial class CreateRecordWindow : Window
+    {
+        public CreateRecordWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListStructuringButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HaphazardIdeasButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ViewSampleTemplatesButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
