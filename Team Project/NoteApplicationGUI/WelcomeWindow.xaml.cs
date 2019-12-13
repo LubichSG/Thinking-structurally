9d25f17b8ece0267c932c7be693565fa63c01881
Анастасия Шапарева <shaparevaas@outlook.com>
2019-12-13 16:39:23 +0300

    created welcome window (still lacks proper design..)

---
+++ Team Project/NoteApplicationGUI/WelcomeWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NoteApplicationGUI
+{
+    /// <summary>
+    /// Логика взаимодействия для WelcomeWindow.xaml
+    /// </summary>
+    public partial class WelcomeWindow : Window
+    {
+        public WelcomeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
