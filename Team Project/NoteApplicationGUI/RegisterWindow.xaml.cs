4bd4185b3736165f2bfdcb5c485246df00f08e03
Анастасия Шапарева <shaparevaas@outlook.com>
2019-12-13 17:03:57 +0300

    Создала окно регистрации (пользователь вводит имя, фамилию, телефон, логину автоматически причисляется значение телефона, пароль и подтверждение пароля)

---
+++ Team Project/NoteApplicationGUI/RegisterWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NoteApplicationGUI
+{
+    /// <summary>
+    /// Логика взаимодействия для RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UnregisterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
