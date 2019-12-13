04d22c3d3e19618849d655e8f74cfd093f73ae3c
Анастасия Шапарева <shaparevaas@outlook.com>
2019-12-13 21:14:31 +0300

    implemented the logic of registration window

--- Team Project/NoteApplicationGUI/RegisterWindow.xaml.cs
+++ Team Project/NoteApplicationGUI/RegisterWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Team_Project;
 
 namespace NoteApplicationGUI
 {
@@ -19,6 +20,7 @@ namespace NoteApplicationGUI
     /// </summary>
     public partial class RegisterWindow : Window
     {
+        UserManager userManager = new UserManager();
         public RegisterWindow()
         {
             InitializeComponent();
@@ -26,12 +28,25 @@ namespace NoteApplicationGUI
 
         private void UnregisterButton_Click(object sender, RoutedEventArgs e)
         {
-
+            WelcomeWindow welcomeWindow = new WelcomeWindow();
+            welcomeWindow.Show();
+            this.Close();
         }
 
         private void RegisterButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (String.IsNullOrEmpty(NameTextBox.Text) || String.IsNullOrEmpty(SurnameTextBox.Text) ||
+                String.IsNullOrEmpty(PasswordConfirmTextBox.Password) ||
+                String.IsNullOrEmpty(PhoneNumberTextBox.Text) || String.IsNullOrEmpty(PasswordTextBox.Password))
+            {
+                MessageBox.Show("All blanks should be filled!");
+            }
+            else if(CheckUserInput(NameTextBox.Text, SurnameTextBox.Text, PhoneNumberTextBox.Text, PasswordTextBox.Password, PasswordConfirmTextBox.Password))
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
         }
 
         private bool CheckUserInput(string name, string surname, string phone, string password, string passwordCheck)
