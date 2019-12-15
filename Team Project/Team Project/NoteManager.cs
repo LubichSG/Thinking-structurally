d94bbb9b9191d05f8ad678008927d7f9735ebe8b
Анастасия Шапарева <shaparevaas@outlook.com>
2019-12-15 14:11:51 +0300

    добавила в note manager метода для работы с файлами

--- Team Project/Team Project/NoteManager.cs
+++ Team Project/Team Project/NoteManager.cs
@@ -1,11 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
+using System.Xml;
+using Newtonsoft.Json;
 
 namespace Team_Project
 {
     public class NoteManager
     {
+        private List<Note> notes;
 
+        private const string NotesFileName = "data/notes.json";
+
+        private T Deserialize<T>(string fileName)
+        {
+            using (var sr = new StreamReader(fileName))
+            {
+                using (var jsonReader = new JsonTextReader(sr))
+                {
+                    var serializer = new JsonSerializer();
+                    return serializer.Deserialize<T>(jsonReader);
+                }
+            }
+        }
+
+        private void Serialize<T>(string fileName, T data)
+        {
+            using (var sw = new StreamWriter(fileName))
+            {
+                using (var jsonWriter = new JsonTextWriter(sw))
+                {
+                    jsonWriter.Formatting = Newtonsoft.Json.Formatting.Indented;
+                    var serializer = new JsonSerializer();
+                    serializer.Serialize(jsonWriter, data);
+                }
+            }
+        }
+
+        private void LoadData()
+        {
+            notes = Deserialize<List<Note>>(NotesFileName);
+        }
     }
 }
