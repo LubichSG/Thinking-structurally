33cb04b94c5db685926fac0996251f071006363e
Анастасия Шапарева <shaparevaas@outlook.com>
2019-12-16 08:50:00 +0300

    ничего особенного

--- Team Project/NoteApplicationGUI/ViewRecordsPage1.xaml.cs
+++ Team Project/NoteApplicationGUI/ViewRecordsPage1.xaml.cs
@@ -1,26 +1,31 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
-//using System.Windows;
-//using System.Windows.Controls;
-//using System.Windows.Data;
-//using System.Windows.Documents;
-//using System.Windows.Input;
-//using System.Windows.Media;
-//using System.Windows.Media.Imaging;
-//using System.Windows.Navigation;
-//using System.Windows.Shapes;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-//namespace NoteApplicationGUI
-//{
-//    /// <summary>
-//    /// Логика взаимодействия для ViewRecordsPage1.xaml
-//    /// </summary>
-//    public partial class ViewRecordsPage1 : Page
-//    {
-//        public ViewRecordsPage1()
-//        {
-//            InitializeComponent();
-//        }
-//    }
-//}
+namespace NoteApplicationGUI
+{
+    /// <summary>
+    /// Логика взаимодействия для ViewRecordsPage1.xaml
+    /// </summary>
+    public partial class ViewRecordsPage1 : Page
+    {
+        public ViewRecordsPage1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNote_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
